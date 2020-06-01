75202d971e79df38433e8ab07e618f21363f1e07
christian <chrisoftech@gmail.com>
2020-06-01 09:52:44 +0000

    One more test for wrong public-key signature verification.

--- test/generate_test.dart
+++ test/generate_test.dart
@@ -30,8 +30,7 @@ void main() {
       _generatedKeyPair = secp256k1KeyPair(_secureRandom);
     });
 
-    test('should return true if signed text is verifiable with public-key',
-        () {
+    test('should return true if signed text is verifiable with public-key', () {
       final _signature =
           _generator.generateSignature(_tMessage, _generatedKeyPair.privateKey);
 
@@ -42,15 +41,32 @@ void main() {
       expect(_isSignedByPrimaryKey, equals(true));
     });
 
-    test('should return false if presumably signed text is not verifiable with public-key',
+    test(
+        'should return false if presumably signed text is not verifiable with public-key',
         () {
-          final _newTestMessage = '$_tMessage added to change signed message';
+      final _newTestMessage = '$_tMessage added to change signed message';
 
       final _signature =
           _generator.generateSignature(_tMessage, _generatedKeyPair.privateKey);
 
       final _isSignedByPrimaryKey = _generator.verifySignature(
           _newTestMessage, _generatedKeyPair.publicKey, _signature);
+
+      expect(_signature, TypeMatcher<ECSignature>());
+      expect(_isSignedByPrimaryKey, equals(false));
+    });
+
+    test(
+        'should return false if signed text is not verifiable with the wrong public-key',
+        () {
+      final _signature =
+          _generator.generateSignature(_tMessage, _generatedKeyPair.privateKey);
+
+      // generated another key-pair
+      final _differentGeneratedKeyPair = secp256k1KeyPair(_secureRandom);
+
+      final _isSignedByPrimaryKey = _generator.verifySignature(
+          _tMessage, _differentGeneratedKeyPair.publicKey, _signature);
 
       expect(_signature, TypeMatcher<ECSignature>());
       expect(_isSignedByPrimaryKey, equals(false));
